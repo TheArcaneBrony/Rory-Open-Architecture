c57d3cb39f858605a5acfc670376eb2b1c8f655d
TheArcaneBrony <myrainbowdash949@gmail.com>
2022-12-26 19:47:54 +0100

    Fix pg_hba
    Signed-off-by: TheArcaneBrony <myrainbowdash949@gmail.com>

--- host/Rory-postgres/configuration.nix
+++ host/Rory-postgres/configuration.nix
@@ -29,7 +29,7 @@
       host all all 127.0.0.1/32 trust
       host all all ::1/128 trust
       host discordbots discordbots 192.168.1.1/24 trust
-      host all all 0.0.0.0 md5
+      host all all 0.0.0.0/0 md5
     '';
     # initialScript = pkgs.writeText "backend-initScript" ''
     #   CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
