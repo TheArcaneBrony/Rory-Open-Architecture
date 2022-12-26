e3a3a1e4d3882a81dc7d682a639e337bb5dc8c1a
TheArcaneBrony <myrainbowdash949@gmail.com>
2022-12-26 19:10:38 +0100

    Postgres setup
    Signed-off-by: TheArcaneBrony <myrainbowdash949@gmail.com>

--- host/Rory-postgres/configuration.nix
+++ host/Rory-postgres/configuration.nix
@@ -14,8 +14,29 @@
     } ];
   };
 
-  environment.systemPackages = with pkgs; [
-  ];
+  # environment.systemPackages = with pkgs; [
+  #   postgresql
+  # ];
+
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_14;
+    enableTCPIP = true;
+    authentication = pkgs.lib.mkOverride 10 ''
+      # TYPE, DATABASE, USER, ADDRESS, METHOD
+      local all all trust
+      host all all 127.0.0.1/32 trust
+      host all all ::1/128 trust
+      host discordbots discordbots 192.168.1.1/24 trust
+      host all all 0.0.0.0 md5
+    '';
+    # initialScript = pkgs.writeText "backend-initScript" ''
+    #   CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
+    #   CREATE DATABASE nixcloud;
+    #   GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
+    # '';
+    dataDir = "/data";
+  };
 
   system.stateVersion = "22.11"; # DO NOT EDIT!
 }
